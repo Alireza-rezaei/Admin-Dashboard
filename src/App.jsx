31015ca20909d99080f8257f22caa27943a67ab7
Alireza Rezaei <alireza_rezaei_2005@yahoo.com>
2023-06-24 18:41:37 +0330

    Creating header section files and completing this section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import ROUTES from './Routes';
 import { useRoutes } from 'react-router-dom';
+import Header from './Components/Header/Header';
 
 function App() {
 	const routes = useRoutes(ROUTES);
-	return <div>{routes}</div>;
+	return (
+		<>
+			<Header />
+			{routes}
+		</>
+	);
 }
 
 export default App;
