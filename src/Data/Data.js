e843c95cdfc8d4b4ad8a00ae9e69d149d808cdf0
Alireza Rezaei <alireza_rezaei_2005@yahoo.com>
2023-06-27 22:27:05 +0330

    Install the ReChart package and use it in the Chart component and add the chart to the home page

---
+++ src/Data/Data.js
@@ -0,0 +1,16 @@
+const xAxisData = [
+	{ name: 'Jan', sales: 112_000 },
+	{ name: 'Feb', sales: 99_000 },
+	{ name: 'Mar', sales: 12_090 },
+	{ name: 'Apr', sales: 99_000 },
+	{ name: 'May', sales: 54_000 },
+	{ name: 'Jun', sales: 85_000 },
+	{ name: 'Jul', sales: 34_000 },
+	{ name: 'Agu', sales: 18_598 },
+	{ name: 'Sep', sales: 0 },
+	{ name: 'Oct', sales: 73_078 },
+	{ name: 'Nov', sales: 12_900 },
+	{ name: 'Dev', sales: 97_000 },
+];
+
+export { xAxisData };
